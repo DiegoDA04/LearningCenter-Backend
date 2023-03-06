c697cfe84cd5e41950b98d7c46045758c27482fd
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2023-03-05 23:20:34 -0500

    Initial commit

---
+++ src/main/java/com/nerv/learningcenterbackend/LearningCenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nerv.learningcenterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCenterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nerv/learningcenterbackend/LearningCenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerv.learningcenterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCenterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
